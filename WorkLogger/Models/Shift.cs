2b6cdb1c61347e189ac8a5c5b9e9bf3d700c082a
Carl Emil Lidbom <lidbom.calle@outlook.com>
2021-12-22 01:40:04 +0100

    Added the shift class, PoC in main. TODO: Container class, json-on/off.

---
+++ WorkLogger/Models/Shift.cs
@@ -0,0 +1,45 @@
+﻿namespace WorkLogger.Models;
+
+public class Shift
+{
+    private DateTime StartTime { get; set; }
+    private DateTime EndTime { get; set; }
+    private TimeSpan TimeWorked { get; set; }
+    private string? Message { get; set; }
+
+    public void Start()
+    {
+        StartTime = DateTime.UtcNow;
+    }
+
+    public void End()
+    {
+        EndTime = DateTime.UtcNow;
+        SetTimeWorked();
+    }
+
+    public DateTime GetStart()
+    {
+        return StartTime;
+    }
+
+    public DateTime GetEnd()
+    {
+        return EndTime;
+    }
+
+    private void SetTimeWorked()
+    {
+        TimeWorked = EndTime - StartTime;
+    }
+
+    public void SetMessage(string message)
+    {
+        Message = message;
+    }
+
+    public override string ToString()
+    {
+        return $"Start time: {StartTime}, End time: {EndTime}, Duration: {TimeWorked}\r\nMessage: {Message}";
+    }
+}
--- WorkLogger/Program.cs
+++ WorkLogger/Program.cs
@@ -1,3 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using WorkLogger.Models;
+
 Console.WriteLine("Hello, World!");
+
+var shift = new Shift();
+shift.Start();
+Thread.Sleep(50000);
+shift.End();
+shift.SetMessage("Test!");
+Console.WriteLine(shift.ToString());
